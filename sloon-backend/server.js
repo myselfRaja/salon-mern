9b12379f3c2c5b753046126c453c9993cefefc5d
Mohammed Raza <mohammedraza11239@gmail.com>
2025-05-12 19:41:37 +0530

    Updated CORS for frontend render domain

--- sloon-backend/server.js
+++ sloon-backend/server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://salon-frontend-cwz0.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
